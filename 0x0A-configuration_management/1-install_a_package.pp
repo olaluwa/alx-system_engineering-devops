8b81292973cce3fd9b2e782b54b6c92d3f4ed6f1
olaluwa <99435322+olaluwa@users.noreply.github.com>
2022-09-21 21:11:47 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# installing puppet link
-package { 'puppet-lint':
-    ensure   => '2.1.1',
-    provider => 'gem',
+# Using puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'gem',
 }
